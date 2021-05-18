9882ac85dccf015a074257a9487860846ce213f7
Mariana <cnh.mariana@gmail.com>
2021-05-18 14:00:35 +0200

    Ajout du local storage

--- javascript/produit.js
+++ javascript/produit.js
@@ -11,13 +11,20 @@ console.log(camId);
 
 const productApi = 'http://localhost:3000/api/cameras';
 
+// fonction qui contient le fetch pour récupérer les données de l'API et avoir l'identifiant du produit dans URL
 function productFetch() {
     fetch(productApi + "/" + camId)
     .then(res => res.json()) 
     .catch((error) => console.log(error))
     .then((data) => {
         if (data){
-            showProduct(data)
+            showProduct(data);
+            const btn = document.querySelector(".btn-panier");
+            btn.addEventListener("click",(ev) => {
+            ev.preventDefault();
+            console.log("fonctionne");
+            ajoutDuProduit(data);
+      });
         }
     });
 }
@@ -36,14 +43,14 @@ function showProduct(camera) {
         <select name="optionProduit" id="optionProduit">${this.showOptionLenses(camera.lenses)}
         </select>
         </form>
-      <button class="btn-envoyer">Commander l'article</button>
+        <button type="submit" class="btn-panier">Commander ici</button>
       </article>`;
 }
 productFetch();
 
 
 
-//Boucle et itération pour avoir les options de lentilles
+//Boucle for et itération pour avoir les options de lentilles
 function showOptionLenses(lenses) {
     let optionLenses = '';
     for (let i = 0, size = lenses.length; i < size; i++) {
@@ -52,156 +59,60 @@ function showOptionLenses(lenses) {
     return optionLenses;
 };
 
-//showOptionLenses();
+function ajoutDuProduit(camera){
 
-// const lenses = document.getElementById("optionProduit")
+    let monPanier = [];
+    let monLocal = JSON.parse(localStorage.getItem("monLocal"));
+    const optionsLentilles = document.querySelector("#optionProduit");
+    const choixLenses = optionsLentilles.value;
 
-// function init(data) {
-// 	document.querySelector("#optionProduit").innerHTML = `<form>
-//     <label for="optionProduit">Lentilles</label>
-//     <select name="optionProduit" id="optionProduit"></select>
-//     </form>`;
-//   data.forEach(data => {
-//     const $opt =  document.createElement("option");
-//     $opt.value = data.value;
-//     $opt.textContent = data.label;
-//     $select.appendChild($opt);
-// 	})
+    //objet avec chaque propriété de produit
+ let camerasProduit = {
+    _id : camera._id,
+    name : camera.name,
+    price : camera.price,
+    imageUrl : camera.imageUrl,
+    qte : 1,
+    option : choixLenses,
+};
+// push dans mon local storage pour récup les info de mon objet
+if (monLocal === null) {
+    monPanier.push(camerasProduit);
+    localStorage.setItem("monLocal", JSON.stringify(monPanier));
+}
+//ajouter le produit dans le panier 
+// if (monLocal[camera._id] !== null) {
+//     monLocal[camera._id].qte += 1;
+// } else {
+//     monLocal[camera._id] = monLocal;
 // }
+};
 
-// init(data);
 
 
-// showProduct().then(data => {
-//     let option;
-//     Object.entries(data.showProduct).forEach(lenses => {
-//         option = document.createElement("option");
-//         option.text = lenses[0];
+// actualiserPanier = () => {
+//     console.log(creationItem);
+// }
+// var bouton = document.getElementsByClassName('.btn-panier');
+
+// for(var i = 0; i < bouton.length; i++) {
+//     bouton[i].addEventListener("click",function(){
+//         let key = this.getAttribute('key');
+//         creationItem[key].quantite++;
+//         actualiserPanier();
+//         return false;
+//     })
+// };
+
+// const envoyerPanier = document.querySelector(".btn-panier");
+
+// envoyerPanier.addEventListener("click", (event) => {
+//     event.preventDefault();
+
+//     const choixProduit = camId.value;
+//     console.log(choixProduit);
+
+//     let optionProduit = {
         
-//     });
-// } );
-
-
-
-//----------------------------------------
-// // Useful functions to have
-
-// // Function to talk to the server and return a bunch of products
-// async function getAllProducts() {
-//     var products = [];
-//     var cards = [];
-//     const errorMessage = "<p>We don't currently have any items in stock, but feel free to add your own product/s </p>";
-    
-//     await fetch('http://localhost:3000/api/cameras', {
-//         method: 'GET',
-//         mode: 'cors'
-//     })
-//     .then(response => response.json())
-//     .then(data => products = data)
-//     .catch(_ => cards.push(errorMessage));
-
-//     if (products.length > 0 && cards.length < 1) {
-//         for (const index in products) {
-//             var product = products[index];
-        
-//             var productID = product.id;
-//             var productName = product.name;
-//             var productImageURL = product.imageURL;
-//             var productPrice = product.price;
-//             var productDescription = product.description;
-
-//             var card = `
-//                 <div id=${productID}>
-//                     <div class="card bg-dark" style="width: 18rem;">
-//                         <img class="card-img-top" src=${productImageURL} alt="Product Image">
-//                         <div class="card-body text-white">
-//                             <h5 class="card-title">${productName}</h5>
-//                             <p class="card-text">${productDescription}</p>
-//                             <br />
-//                             <p><strong>Price: $</strong> ${productPrice}</p>
-//                         </div>
-//                         <div class="card-footer bg-transparent text-center row">
-//                             <button type="button" class="btn btn-outline-warning btn-sm col" id="buy-btn">Buy Product</button>
-//                             <button type="button" class="btn btn-outline-warning btn-sm col offset-md-1" id="edit-btn">Edit Product</button>
-//                         </div>
-//                     </div>
-//                 </div>
-//             `;
-
-//             cards.push(card);
-//         }
-//     } else if (products.length < 1 && cards.length < 1) {
-//         cards.push(errorMessage);
 //     }
-
-//     return cards;
-// }
-
-// Create a new product
-// async function createNewProduct(product) {
-//     var result = false;
-
-//     await fetch('http://localhost:3000/api/cameras', {
-//         method: 'POST',
-//         body: JSON.stringify(product),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     })
-//     .then(_ => result = true)
-//     .catch(_ => result = false);
-
-//     return result;
-// }
-
-// // Update product
-// async function updateProduct(camId, product) {
-//     var result = false;
-
-//     await fetch(`http://localhost:3000/api/cameras/${id}`, {
-//         method: 'POST',
-//         body: JSON.stringify(product),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     })
-//     .then(_ => result = true)
-//     .catch(_ => result = false);
-
-//     return result;
-// }
-
-// // Get a single product
-// async function getProduct(id) {
-//     var products = [];
-    
-//     await fetch('http://localhost:3000/api/cameras', {
-//         method: 'GET',
-//         mode: 'cors'
-//     })
-//     .then(response => response.json())
-//     .then(data => products = data)
-//     .catch(_ => {});
-
-//     if (products.length > 0) {
-//         for (const index in products) {
-//             var product = products[index];
-
-//             if (product.id == id) {
-//                 console.log("Product match!");
-//                 return {
-//                     "name": product.name,
-//                     "imageURL": product.imageURL,
-//                     "price": product.price,
-//                     "description": product.description
-//                 };
-//             }
-//         }
-//     }
-
-//     return {};
-// }
-
-//Afficher le produit sélectionné par l'id (objet) avec .find()
-//const idProduitSelectionner = url.find( element => element._id === _id);
-//console.log(idProduitSelectionner)
+// })
